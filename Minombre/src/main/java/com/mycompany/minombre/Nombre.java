56ac642267d78911ece912c2c2cd81d75b12d7f5
Carlos Alejandro Maravilla Perez <carlos.maravilla1176@uppuebla.edu.mx>
2020-05-16 15:51:32 -0500

    prueba

---
+++ Minombre/src/main/java/com/mycompany/minombre/Nombre.java
@@ -0,0 +1,14 @@
+
+
+import java.util.*;
+import java.util.Scanner;
+
+
+public class Nombre {
+    
+    public static void main(String[]args){
+
+    System.out.println("Carlos Alejndro Maravilla Perez");
+}
+    
+}
