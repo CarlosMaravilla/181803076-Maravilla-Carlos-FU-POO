ebe7a7a9fc67c5384fe0e877d81e15dd11d780e2
Carlos Alejandro Maravilla Perez <carlos.maravilla1176@uppuebl.edu.mx>
2020-05-21 09:33:07 -0500

    Poligono

--- Poligono/src/main/java/Poligono.java
+++ Poligono/src/main/java/Poligono.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 
 public class Poligono {
     public static void main(String[]args){
-        double perimetro=0, apotema=0,area=0;
-        
+        double lado=0, apotema=0,area=0,n=0;
+
         Scanner leer = new Scanner(System.in);
-        System.out.println("ingrese el perimetro");
-        perimetro = leer.nextDouble();
+        System.out.println("ingrese el numero de lados");
+        n = leer.nextDouble();       
+        System.out.println("ingrese la medida del lado");
+        lado = leer.nextDouble();
         System.out.println("ingrese el apotema");
         apotema = leer.nextDouble();
-        
-        area=(perimetro*apotema)/2;
-        
+
+        area=((n*lado)*apotema)/2;
+
         System.out.println("El area del poligono es "+area);
     }
 }
