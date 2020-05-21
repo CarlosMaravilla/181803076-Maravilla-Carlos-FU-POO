a33436fc9aad380f6c0cdbffc963237a07468514
Carlos Alejandro Maravilla Perez <carlos.maravilla1176@uppuebl.edu.mx>
2020-05-21 08:37:36 -0500

    Cuadrado

---
+++ Cuadrado/src/main/java/Cuadrado.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Cuadrado {
+    public static void main(String[]args){
+        
+        double L1 = 0,L2;
+        
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el lado del cuadrado");
+        
+         L1 = leer.nextDouble();
+        
+        System.out.println("El area es "+(L1*L1));
+        
+        
+        
+        
+    }
+    
+    
+}
